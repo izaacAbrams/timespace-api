6470c3c815fc00f074fea9e4d322be7d48bbed0c
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-09 19:17:45 -0500

    enabled cors

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
 };
