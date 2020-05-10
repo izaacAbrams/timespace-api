876d2e4dc552e8eafd283f9282dfdf769f95219f
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-09 19:29:08 -0500

    added authentication and test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const logger = require("./logger");
 const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 
 const app = express();
@@ -16,6 +17,17 @@ app.use(
     origin: CLIENT_ORIGIN,
   })
 );
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
 
 app.get("/api/*", (req, res) => {
   res.send({ ok: true });
--- test/app.spec.js
+++ test/app.spec.js
@@ -2,6 +2,9 @@ const app = require("../src/app");
 
 describe("App", () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app).get("/api/").expect(200, { ok: true });
+    return supertest(app)
+      .get("/api/")
+      .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+      .expect(200, { ok: true });
   });
 });
