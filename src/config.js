a9acdd9c0490d52355fed6e4ab916e92f58b7e40
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-12 20:56:31 -0500

    fixed package json

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
-  DATABSE_URL:
+  DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/timespace",
 };
