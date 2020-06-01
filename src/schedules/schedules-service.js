f2e696bb78bc6e2b7f14c2863598131ebaf0171e
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-31 21:07:14 -0500

    fixed problem with delete schedules

--- src/schedules/schedules-service.js
+++ src/schedules/schedules-service.js
@@ -33,7 +33,7 @@ const SchedulesService = {
   },
 
   deleteSchedule(knex, id) {
-    return knex("timespace_schedules").where({ schedule_url }).delete();
+    return knex("timespace_schedules").where({ id }).delete();
   },
 
   updateSchedule(knex, id, newScheduleFields) {
